3091070ed1b8c2fa759575247d4ea0b4f93e06f7
Amadou Niangaly <amadouniangaly16@gmail.com>
2024-06-04 11:10:21 +0000

    mon commit 2

--- src/main/java/com/mycontact/servlets/contactServlet.java
+++ src/main/java/com/mycontact/servlets/contactServlet.java
@@ -6,10 +6,11 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+@WebServlet(urlPatterns = {"/contacts", "/addContact", "/editContact", "/updateContact", "/deleteContact"})
 public class contactServlet extends HttpServlet {
 
     private List<Contact> contacts = new ArrayList<>();
